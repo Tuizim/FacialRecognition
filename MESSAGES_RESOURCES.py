e3058f73de55a7f72fb52a417378ff02911717ef
Tuizim <tuitrombim@gmail.com>
2024-11-08 11:20:00 -0300

    [UPD][NEW] Criação de novos metodos e melhoria no fluxo

---
+++ MESSAGES_RESOURCES.py
@@ -0,0 +1,11 @@
+class Messages:
+    class Errors:
+        GENERIC_ERROR= "Error"
+        class CameraError:
+            NOT_OPPENED = "Erro ao abrir a camera"
+            CAPTURE_FAIL ="Falha na captura"
+
+    class Success:
+        GENERIC_SUCCESS= "Sucesso!"
+    class Warming:
+        GENERIC_WARMING= "Atenção!"
